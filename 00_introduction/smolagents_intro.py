bf1990d27bf65d3926208f93ea76990915c22172
PabloRR100 <pabloruizruiz10@gmail.com>
2025-02-13 19:27:01 +0100

    Intro to smolagents library

---
+++ 00_introduction/smolagents_intro.py
@@ -0,0 +1,35 @@
+import datetime
+import requests
+import pytz
+import yaml
+
+from tools.final_answer import FinalAnswerTool
+
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool
+
+
+@tool
+def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
+    #Keep this format for the description / args / args description but feel free to modify the tool
+    """A tool that does nothing yet 
+    Args:
+        arg1: the first argument
+        arg2: the second argument
+    """
+    return "What magic will you build ?"
+
+
+@tool
+def get_current_time_in_timezone(timezone: str) -> str:
+    """A tool that fetches the current local time in a specified timezone.
+    Args:
+        timezone: A string representing a valid timezone (e.g., 'America/New_York').
+    """
+    try:
+        # Create timezone object
+        tz = pytz.timezone(timezone)
+        # Get current time in that timezone
+        local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return f"The current local time in {timezone} is: {local_time}"
+    except Exception as e:
+        return f"Error fetching time for timezone '{timezone}': {str(e)}"
